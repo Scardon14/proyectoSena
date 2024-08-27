4d364826b6f87c112fb43669e87940d79c1b6073
Scardon14 <sara.gcardonat@gmail.com>
2024-08-27 18:06:22 -0500

    se hace modificacion en el codigo de inicio donde se reubicaron archivos a carpetas correspondientes (estilo.css y logo), se envia imagen de barberia a la carpeta correspondiente (img), se elimina la que crearon en carpeta view. Se hace actualizacion de registros por el momento se espera confirmacion del profesor para eliminar los otros archivos.

---
+++ js/registro.js
@@ -0,0 +1,64 @@
+/* function mostrarForm(){
+    const elemento = document.getElementById("registro");
+    var pagina = elemento.options[elemento.selectedIndex].value;
+    pagina = pagina + ".html";
+    loadHTML(pagina,"form");
+    const btnRegistrarse=document.getElementById("registrarse");
+    btnRegistrarse.style.display="none";
+    const formulario = document.getElementById("form");
+    formulario.style.display="block";
+}
+
+function loadHTML(url, targetElementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(targetElementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the HTML:', error));
+}
+function ocultarFormulario(){
+    const formulario = document.getElementById("form");
+    formulario.style.display="none";
+    const btnRegistrarse = document.getElementById("registrarse");
+    btnRegistrarse.style.display="block";
+} */
+//JS para formulario unificado
+
+function mostrarForm(){  
+    const btnRegistrarse = document.getElementById("registrarse");
+    const registroBarbero = document.getElementById("registroBarbero");
+    const registroNegocio = document.getElementById("registroNegocio");
+    const registroUsuario = document.getElementById("registroUsuario");
+    const opcionRegistro = document.getElementById("opcionRegistro");    
+    let registroSeleccionado = opcionRegistro.options[opcionRegistro.selectedIndex].value;
+    if (registroSeleccionado=="registroBarbero"){
+        registroNegocio.style.display="none";
+        registroUsuario.style.display="none";
+        registroBarbero.style.display="flex";
+    }else if(registroSeleccionado=="registroNegocio"){
+        registroBarbero.style.display="none";
+        registroUsuario.style.display="none";
+        registroNegocio.style.display="flex";
+    } else if(registroSeleccionado=="registroUsuario"){
+        registroBarbero.style.display="none";
+        registroNegocio.style.display="none";
+        registroUsuario.style.display="flex";
+    }
+    opcionRegistro.style.display="none";
+    btnRegistrarse.style.display="none";
+}
+function ocultarFormulario(){
+    const btnRegistrarse = document.getElementById("registrarse");
+    const registroBarbero = document.getElementById("registroBarbero");
+    const registroNegocio = document.getElementById("registroNegocio");
+    const registroUsuario = document.getElementById("registroUsuario");
+    const opcionRegistro = document.getElementById("opcionRegistro");
+    btnRegistrarse.style.display="block";
+    opcionRegistro.style.display="block";
+    registroBarbero.style.display="none";
+    registroNegocio.style.display="none";
+    registroUsuario.style.display="none";
+
+}
+
