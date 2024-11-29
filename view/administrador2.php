076a7102fbb5299c20ba37a26d27c7f659c3b046
Camilo Vargas <camilo.val1999@gmail.com>
2024-11-28 22:55:14 -0500

    Corrección cambios

--- view/administrador2.php
+++ view/administrador2.php
@@ -16,24 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../css/administrador2.css">
     <title> Administrador</title>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../css/administrador2.css">
-    <title> Administrador</title>
 </head>
-
 
 <Body>
     <div class="main">
@@ -62,33 +45,7 @@
         </header>
         <h1 class="text-center p-3"> ADMINISTRACION</h1> <!-- Titulo visto en la pagina-->
         <div class="container-fluid row">
-        <!--Header-->
-        <header>
-            <div class="contenedor-encabezado"> <!--Encabezado Azul-->
-                <div class="logo">
-                    <img src="../img/logo.png" alt="logo">
-                </div>
-                <div class="nombre">
-                    <h1> O & T </h1>
-                </div>
-            </div>
-            <div class="contenedor_menu"> <!--Encabezado rojo-->
-                <div class="menu">
-                    <nav>
-                        <ul>
-                            <li><a href="../model/listadoClientes.php">Listado de Cliente</a></li>
-                            <li><a href="../model/listadoBarbero.php">Listado de Barberos</a></li>
-                            <li><a href="../model/listadoNegocio.php">Listado de Negocio</a></li>
-                            <li><a href="">Cerrar sesion</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
-        <h1 class="text-center p-3"> ADMINISTRACION</h1> <!-- Titulo visto en la pagina-->
-        <div class="container-fluid row">
             <div class="">
-                <table class="table"> <!-- Fila de titulos-->
                 <table class="table"> <!-- Fila de titulos-->
                     <thead class="">
                         <tr class="">
@@ -115,23 +72,12 @@
                                 <td><?= $cliente['nombres']; ?></td>
                                 <td><?= $cliente['apellidos']; ?></td>
                                 <td><?= $cliente['numeroCelular']; ?></td>
-                                <td><?= $cliente['correoElectronico']; ?></td>
+                                <td><?= $cliente['correoElectronico']; ?>
+                                <td>
                                 <td><?= $cliente['idUsuario']; ?></td>
                                 <td><?= $cliente['fechaCreacion']; ?></td>
                                 <td><?= $cliente['estado']; ?></td>
-
                                 <td>
-                                    <a href="../model/actualizarCliente.php?id=<?= $cliente['idCliente']; ?>"
-                                        class="btn btn-small btn-success"><i class="bi bi-pencil-square">Editar</i></a>
-                                    <!--Boton editar-->
-                                    <form action="../model/eliminarCliente.php" method="POST">
-                                        <div class="idCliente" hidden>
-                                            <input type="text" class="form-control" id="exampleInputname1" name="idCliente"
-                                                aria-describedby="nameHelp" value="<?= $cliente['idCliente'] ?>" hidden>
-                                        </div>
-                                        <button type="submit" class="btn btn-small btn-danger"><i
-                                                class="bi bi-trash3-fill">Eliminar</i></button> <!-- Boton eliminar-->
-                                    </form>
                                     <a href="../model/actualizarCliente.php?id=<?= $cliente['idCliente']; ?>"
                                         class="btn btn-small btn-success"><i class="bi bi-pencil-square">Editar</i></a>
                                     <!--Boton editar-->
@@ -159,10 +105,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
 </Body>
 
-</html>
 </html>
