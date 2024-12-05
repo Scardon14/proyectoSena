443747140bcc7f11804b7765345f72a0e4a9672c
Camilo Vargas <camilo.val1999@gmail.com>
2024-12-05 11:07:12 -0500

    Creacion de eitar fotos

--- view/miperfil_barbero.php
+++ view/miperfil_barbero.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../css/mi_perfil_barbero.css">
     <title>Mi perfil</title>
 </head>
@@ -31,27 +34,40 @@
 
             <div class="galeria">
                 <h3>Galería de Fotos</h3><br>
-                <div class="fotos-contenedor"> <!-- Contenedor para todas las fotos -->
-                    <?php
-                    $fotosTrabajo = $_SESSION['fotos-trabajo'];
-                    foreach ($fotosTrabajo as $foto) { ?>
+                <div idUsuario="<?= $usuario['idUsuario']; ?>" id="idUsuario"></dividU>
+                <?php
+                $fotosTrabajo = $_SESSION['fotos-trabajo'];
+                foreach ($fotosTrabajo as $foto) { ?>
+                    <div class="fotos-contenedor" id="foto-<?=$foto['idFotografia']?>"> <!-- Contenedor para todas las fotos -->
+
                         <div class="foto">
+                            <button idFoto="<?= $foto['idFotografia'] ?>" foto="<?= $foto['foto1'] ?>" campo="foto1"
+                                class="btn btn-small btn-success btn-editar"><i class="bi bi-pencil-square"></i></button>
                             <img src="<?= $foto['foto1'] ?>" alt="Foto 1">
                         </div>
                         <div class="foto">
+                            <button idFoto="<?= $foto['idFotografia'] ?>" foto="<?= $foto['foto2'] ?>" campo="foto2"
+                                class="btn btn-small btn-success btn-editar"><i class="bi bi-pencil-square"></i></button>
                             <img src="<?= $foto['foto2'] ?>" alt="Foto 2">
                         </div>
                         <div class="foto">
+                            <button idFoto="<?= $foto['idFotografia'] ?>" foto="<?= $foto['foto3'] ?>" campo="foto3"
+                                class="btn btn-small btn-success btn-editar"><i class="bi bi-pencil-square"></i></button>
                             <img src="<?= $foto['foto3'] ?>" alt="Foto 3">
                         </div>
                         <div class="foto">
+                            <button idFoto="<?= $foto['idFotografia'] ?>" foto="<?= $foto['foto4'] ?>" campo="foto4"
+                                class="btn btn-small btn-success btn-editar"><i class="bi bi-pencil-square"></i></button>
                             <img src="<?= $foto['foto4'] ?>" alt="Foto 4">
                         </div>
                         <div class="foto">
+                            <button idFoto="<?= $foto['idFotografia'] ?>" foto="<?= $foto['foto5'] ?>" campo="foto5"
+                                class="btn btn-small btn-success btn-editar"><i class="bi bi-pencil-square"></i></button>
                             <img src="<?= $foto['foto5'] ?>" alt="Foto 5">
                         </div>
-                    <?php } ?>
-                </div>
+
+                    </div>
+                <?php } ?>
             </div>
             <script>
                 function guardarCambios() {
@@ -69,6 +85,8 @@
             <!-- Botón para guardar cambios -->
             <button onclick="guardarCambios()">Guardar Cambios</button>
     </section>
+    <script src="../js/fotos.js"></script>
+
     <footer>
         <?php include('../view/footer.html'); ?>
     </footer>
