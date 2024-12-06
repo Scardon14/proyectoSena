102cbd863ba847f3967504ae7722f3c60cec7ae5
Nikog09 <nikoltdahan@gmail.com>
2024-12-05 23:16:53 -0500

    Modificacion diseño

--- view/index.php
+++ view/index.php
@@ -24,56 +24,58 @@
     </script>
     </head>
     <Body> 
-        <header>
-            <div class="contenedor-encabezado">  <!--Encabezado Azul-->
-                <div class="logo">
-                    <img src="../img/logo.png" alt="logo">
+        <div class="main">
+            <header>
+                <div class="contenedor-encabezado">  <!--Encabezado Azul-->
+                    <div class="logo">
+                        <img src="../img/logo.png" alt="logo">
+                    </div>
+                    <div class="nombre">
+                        <h1> O & T </h1>
+                    </div>
                 </div>
-                <div class="nombre">
-                    <h1> O & T </h1>
-                </div>
+            <div class="contenedor_menu"> <!--Encabezado rojo-->
+                    <div class="menu">
+                        <nav>
+                            <ul>
+                            <li><a href="index.php">Inicio</a></li>
+                            <li><a href="../model/ProcesarNegocio.php">Regristrate</a></li>
+                            <li><a href="inicioSesion.php">Inicia sesion</a></li>
+                            <li><a href="quienes_somos.php">Quienes somos?</a></li>
+                            <li><a href="../model/listadoClientes.php">administrador</a></li>
+                            </ul>
+                        </nav>
+                    </div>
             </div>
-           <div class="contenedor_menu"> <!--Encabezado rojo-->
-                <div class="menu">
-                    <nav>
-                        <ul>
-                         <li><a href="index.php">Inicio</a></li>
-                         <li><a href="../model/ProcesarNegocio.php">Regristrate</a></li>
-                         <li><a href="inicioSesion.php">Inicia sesion</a></li>
-                         <li><a href="quienessomos.php">Quienes somos?</a></li>
-                         <li><a href="../model/listadoClientes.php">administrador</a></li>
-                        </ul>
-                    </nav>
+            </header>
+            <section id="presenta">  <!--Seccion de img con titulo-->   
+            <h1 class="org">ORGANIZACION Y TIEMPO</h1>
+            </section> 
+            <section id="organizacion">  <!--Seccion de fondo blanco, img y texto-->
+                <div class="casi">
+                    <div class="casilla"></div>
+                        <div class="texto">
+                            <h1> O&T (OrganizacionyTiempo) </h1>
+                            <P>Prueba Gratis el software que te ayudara a optimizar tiempo y <br>
+                            tu trabajo aumenta tu rentabilidad y automatiza la gestion de agendamiento,<br> 
+                            clientes y pagos en un momento.</P>
+                            <p>Te ayudaremos a resolver: </p>
+                            <nav class="lista">
+                                <ol>
+                                <li>Elegir la barberia de tu preferencia.</li>
+                                <li>Reservar y cancelar citas.</li>
+                                <li>Vinculacion de tus barberos.</li>
+                                <li>Automatiza las tareas.</li>
+                                </ol>
+                            </nav>
+                        </div>
                 </div>
-           </div>
-       </header>
-       <section id="presenta">  <!--Seccion de img con titulo-->   
-       <h1 class="org">ORGANIZACION Y TIEMPO</h1>
-       </section> 
-       <section id="organizacion">  <!--Seccion de fondo blanco, img y texto-->
-          <div class="casi">
-               <div class="casilla"></div>
-                <div class="texto">
-                    <h1> O&T (OrganizacionyTiempo) </h1>
-                    <P>Prueba Gratis el software que te ayudara a optimizar tiempo y <br>
-                     tu trabajo aumenta tu rentabilidad y automatiza la gestion de agendamiento,<br> 
-                     clientes y pagos en un momento.</P>
-                     <p>Te ayudaremos a resolver: </p>
-                     <nav class="lista">
-                         <ol>
-                          <li>Elegir la barberia de tu preferencia.</li>
-                          <li>Reservar y cancelar citas.</li>
-                          <li>Vinculacion de tus barberos.</li>
-                          <li>Automatiza las tareas.</li>
-                         </ol>
-                     </nav>
-                </div>
-          </div>
-       </section>
-       <div>
-       <footer>
-            <?php include('../view/footer.html'); ?>
-        </footer> 
+            </section>
+            <div>
+            <footer>
+                    <?php include('../view/footer.html'); ?>
+                </footer> 
+            </div>
        </div>
     </Body>
 </html>
